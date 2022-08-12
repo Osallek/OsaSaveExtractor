14c7db514a43a72aeb3275c12f2ba70a8a9b303b
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2022-08-13 00:03:15 +0200

    Fix some NPE

--- src/main/java/fr/osallek/osasaveextractor/service/object/save/EstateDTO.java
+++ src/main/java/fr/osallek/osasaveextractor/service/object/save/EstateDTO.java
@@ -1,13 +1,12 @@
 package fr.osallek.osasaveextractor.service.object.save;
 
 import fr.osallek.clausewitzparser.common.ClausewitzUtils;
+import fr.osallek.eu4parser.model.game.EstatePrivilege;
 import fr.osallek.eu4parser.model.save.country.EstateInteraction;
 import fr.osallek.eu4parser.model.save.country.SaveEstate;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 public class EstateDTO {
 
@@ -37,7 +36,12 @@ public class EstateDTO {
         this.influence = estate.getInfluence();
         this.influenceFromTerritory = estate.getInfluenceFromTerritory();
         this.territory = estate.getTerritory();
-        this.grantedPrivileges = estate.getGrantedPrivileges().stream().map(i -> i.getPrivilege().getName()).toList();
+        this.grantedPrivileges = estate.getGrantedPrivileges()
+                                       .stream()
+                                       .map(EstateInteraction::getPrivilege)
+                                       .filter(Objects::nonNull)
+                                       .map(EstatePrivilege::getName)
+                                       .toList();
         this.influenceModifiers = estate.getInfluenceModifiers().stream().map(EstateModifierDTO::new).toList();
         this.loyaltyModifiers = estate.getLoyaltyModifiers().stream().map(EstateModifierDTO::new).toList();
         this.activeInfluences = estate.getActiveInfluences();
