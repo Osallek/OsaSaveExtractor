3e1c49fcc911607bc7e457eb46a760268a18f633
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2023-01-10 19:49:07 +0100

    fix logs folder

--- src/main/java/fr/osallek/osasaveextractor/OsaSaveExtractorApplication.java
+++ src/main/java/fr/osallek/osasaveextractor/OsaSaveExtractorApplication.java
@@ -1,5 +1,6 @@
 package fr.osallek.osasaveextractor;
 
+import fr.osallek.eu4parser.common.Eu4Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OsaSaveExtractorApplication {
 
     public static void main(String[] args) {
+        System.setProperty("OSALLEK_DOCUMENTS", Eu4Utils.OSALLEK_DOCUMENTS_FOLDER.toString());
         SpringApplication.run(OsaSaveExtractorApplication.class, args);
     }
 
